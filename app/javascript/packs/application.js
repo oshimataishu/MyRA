7260549cf17d41a0db364331a646c099e34d7a09
大島泰周 <ooshimayasuchika@ooshimayasuchikanoMacBook-Air.local>
2024-09-14 20:49:26 +0900

    added feature customer session

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'bootstrap';
+import '../stylesheets/application.scss';
+import '../stylesheets/application.css'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  # devise routings
   devise_for :admin
-  devise_for :customers
+  # homes controller
+  root 'homes#top'
+  get 'about' => 'homes#about'
+
+  # customer's module
+  scope module: :public do
+    devise_for :customers
+    # customers controller
+    resource :customers, only: [:show, :edit, :update] do
+      collection do
+        post 'confirm'
+        patch 'unsubscribe'
+      end
+    end
+
+    # 
+
+
+  end
+
+  # admin's namespace
+  namespace :admin do
+
+  end
 end
